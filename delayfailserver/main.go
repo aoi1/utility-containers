ae91f8c1285987d29c53c56d3ca7d24b3c361b55
aoi1 <tkhsaoi88@gmail.com>
2023-10-08 21:22:20 +0900

    Make health endpoint

--- delayfailserver/main.go
+++ delayfailserver/main.go
@@ -1,48 +1,43 @@
 package main
 
 import (
+	"log"
 	"net/http"
-	"sync"
 	"time"
-  "fmt"
 )
 
-var (
-	// Flag to determine whether to send an error or not
-	sendError bool
-	// Mutex to safely update and check the sendError flag
-	mu sync.Mutex
-)
+var startTime time.Time
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	// Lock the mutex and defer its unlock until the function returns
-	mu.Lock()
-	defer mu.Unlock()
-
-	// Check if the server should send an error
-	if sendError {
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-		return
-	}
-
-	// Otherwise, respond normally
 	w.Write([]byte("Hello, world!"))
 }
 
-func main() {
-	// Set a timer to trigger 5 seconds after startup
-	go func() {
-		time.Sleep(10 * time.Second)
-		mu.Lock()
-		defer mu.Unlock()
-		sendError = true
-	}()
-
-	// Set up the HTTP server
-	http.HandleFunc("/", handler)
-	fmt.Println("Server is starting on port 8080...")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		panic(err)
+func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
+	if time.Since(startTime) < 10*time.Second {
+		log.Println("Health Check: OK")
+		w.Write([]byte("OK"))
+	} else {
+		log.Println("Error: Service Unhealthy")
+		// Respond with an error status after 10 seconds have passed
+		http.Error(w, "Service Unhealthy", http.StatusServiceUnavailable)
 	}
 }
 
+func main() {
+	// Record the start time of the server
+	startTime = time.Now()
+
+	http.HandleFunc("/", handler)
+	http.HandleFunc("/healthz", healthCheckHandler)
+
+	srv := &http.Server{
+		Addr:         ":8080",
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+
+	log.Println("Starting server...")
+	if err := srv.ListenAndServe(); err != nil {
+		log.Fatalf("Server failed: %v", err)
+	}
+}
